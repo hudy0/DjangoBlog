6c759434b9b0abe5423530bc93290b34c5285e11
hudy0 <hudaifa15@outlook.com>
2024-07-07 15:30:58 +0300

    add account profile model.

--- django_blog/accounts/admin.py
+++ django_blog/accounts/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from .models import User, Account
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ("username", "email", "first_name", "last_name", "is_active", "is_staff")
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    )
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "full_name",
+    )
--- django_blog/accounts/models.py
+++ django_blog/accounts/models.py
@@ -1,6 +1,16 @@
 # accounts/models.py
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # profile_pic = models.ImageField(default="default.jpg", upload_to="profile_pics", blank=True, null=True)
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.full_name
