f3abbad4c0d642d894da673be6500f47f7f19bd4
hudy0 <hudaifa15@outlook.com>
2024-07-09 00:05:45 +0300

    add login page

--- django_blog/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
-
-
-def account_list(request: HttpRequest) -> HttpResponse:
-    template_name = "accounts/account_list.html"
-    return render(request, template_name)
