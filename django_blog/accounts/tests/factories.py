f3d9904bbe230741e8e0281f5363dbeeef313ccf
hudy0 <hudaifa15@outlook.com>
2024-07-09 01:05:57 +0300

    add a test for the account model

--- django_blog/accounts/tests/factories.py
+++ django_blog/accounts/tests/factories.py
@@ -1,10 +1,12 @@
 import factory
 from django.db.models.signals import post_save
 
+from django_blog.accounts.models import User, Account
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = "accounts.User"
+        model = User
 
     username = factory.Sequence(lambda n: f"user{n}")
     email = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
@@ -13,6 +15,6 @@ class UserFactory(factory.django.DjangoModelFactory):
 @factory.django.mute_signals(post_save)
 class AccountFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = "accounts.Account"
+        model = Account
 
-    user = factory.SubFactory("django_blog.accounts.tests.factories.UserFactory")
+    user = factory.SubFactory(UserFactory)
