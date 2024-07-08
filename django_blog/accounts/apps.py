80d6e6fbe91d9ecc5f49bd07a259640caed37a38
hudy0 <hudaifa15@outlook.com>
2024-07-09 01:02:01 +0300

    add a test for the account model

--- django_blog/accounts/apps.py
+++ django_blog/accounts/apps.py
@@ -5,8 +5,8 @@ class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_blog.accounts"
 
-    def ready(self):
-        try:
-            import django_blog.accounts.signals  # noqa: F401
-        except ImportError:
-            pass
+    # def ready(self):
+    #     try:
+    #         import django_blog.accounts.signals  # noqa: F401
+    #     except ImportError:
+    #         pass
